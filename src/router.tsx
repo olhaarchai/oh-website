caa54bb0a570c0be74b67f34073c85ac827191cb
ho <olha.arch.ai@gmail.com>
2026-04-21 21:50:43 +0300

    changs

--- src/router.tsx
+++ src/router.tsx
@@ -1,20 +1,17 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createHashRouter } from 'react-router-dom'
 import Layout from './components/Layout'
 import AboutPage from './pages/AboutPage'
 import ProjectsPage from './pages/ProjectsPage'
 import ReviewerPage from './pages/ReviewerPage'
 
-export const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <Layout />,
-      children: [
-        { index: true, element: <AboutPage /> },
-        { path: 'projects', element: <ProjectsPage /> },
-        { path: 'projects/reviewer', element: <ReviewerPage /> },
-      ],
-    },
-  ],
-  { basename: '/oh-website' },
-)
+export const router = createHashRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { index: true, element: <AboutPage /> },
+      { path: 'projects', element: <ProjectsPage /> },
+      { path: 'projects/reviewer', element: <ReviewerPage /> },
+    ],
+  },
+])
